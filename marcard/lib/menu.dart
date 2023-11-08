373c3d1fa1fbe84c2d4bab1fe578ad2729627c60
gammafarrel <gamma0843@gmail.com>
2023-11-08 09:03:35 +0700

    tugas7: bonus done

--- marcard/lib/menu.dart
+++ marcard/lib/menu.dart
@@ -4,58 +4,60 @@ class MyHomePage extends StatelessWidget {
   MyHomePage({Key? key}) : super(key: key);
 
   final List<ShopItem> items = [
-    ShopItem("Lihat Produk", Icons.checklist),
-    ShopItem("Tambah Produk", Icons.add_shopping_cart),
-    ShopItem("Logout", Icons.logout),
+    ShopItem("Lihat Produk", Icons.checklist, Colors.blue),
+    ShopItem("Tambah Produk", Icons.add_shopping_cart, Colors.red),
+    ShopItem("Logout", Icons.logout, Colors.green),
   ];
 
   @override
-    Widget build(BuildContext context) {
-        return Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'Marcard',
-          ),
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Marcard',
+          style: TextStyle(color: Color.fromARGB(255, 212, 0, 0)),
         ),
-        body: SingleChildScrollView(
-          // Widget wrapper yang dapat discroll
-          child: Padding(
-            padding: const EdgeInsets.all(10.0), // Set padding dari halaman
-            child: Column(
-              // Widget untuk menampilkan children secara vertikal
-              children: <Widget>[
-                const Padding(
-                  padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
-                  // Widget Text untuk menampilkan tulisan dengan alignment center dan style yang sesuai
-                  child: Text(
-                    'Welcome To Marcard', // Text yang menandakan toko
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                    ),
+        backgroundColor: const Color.fromARGB(255, 157, 171, 255),
+      ),
+      body: SingleChildScrollView(
+        // Widget wrapper yang dapat discroll
+        child: Padding(
+          padding: const EdgeInsets.all(10.0), // Set padding dari halaman
+          child: Column(
+            // Widget untuk menampilkan children secara vertikal
+            children: <Widget>[
+              const Padding(
+                padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                // Widget Text untuk menampilkan tulisan dengan alignment center dan style yang sesuai
+                child: Text(
+                  'Welcome To Marcard', // Text yang menandakan toko
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-                // Grid layout
-                GridView.count(
-                  // Container pada card kita.
-                  primary: true,
-                  padding: const EdgeInsets.all(20),
-                  crossAxisSpacing: 10,
-                  mainAxisSpacing: 10,
-                  crossAxisCount: 3,
-                  shrinkWrap: true,
-                  children: items.map((ShopItem item) {
-                    // Iterasi untuk setiap item
-                    return ShopCard(item);
-                  }).toList(),
-                ),
-              ],
-            ),
+              ),
+              // Grid layout
+              GridView.count(
+                // Container pada card kita.
+                primary: true,
+                padding: const EdgeInsets.all(20),
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                crossAxisCount: 3,
+                shrinkWrap: true,
+                children: items.map((ShopItem item) {
+                  // Iterasi untuk setiap item
+                  return ShopCard(item);
+                }).toList(),
+              ),
+            ],
           ),
         ),
-      );
-    }
+      ),
+    );
+  }
 }
 
 class ShopCard extends StatelessWidget {
@@ -66,7 +68,7 @@ class ShopCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      color: Colors.indigo,
+      color: item.color,
       child: InkWell(
         // Area responsive terhadap sentuhan
         onTap: () {
@@ -106,6 +108,7 @@ class ShopCard extends StatelessWidget {
 class ShopItem {
   final String name;
   final IconData icon;
+  final Color color;
 
-  ShopItem(this.name, this.icon);
+  ShopItem(this.name, this.icon, this.color);
 }
